503f76ac069f0d3c73c93d520e1b16f549e566f6
Sudhanshu Singh <sudhanshusingh04251@gmail.com>
2025-12-29 14:26:54 +0000

    conversion of data types

---
+++ 01_basics/03_conversionOperation.js
@@ -0,0 +1,28 @@
+let score = null
+
+//console.log(typeof score);
+//console.log(typeof(score));
+
+let valueInNumber =Number(score)
+console.log(typeof valueInNumber);
+console.log(valueInNumber);
+
+// "33" => 33
+// "33abc" => NaN
+// true => 1; false => 0;
+
+let isLoggedIn = true;
+
+let booleanIsLoggedIn = Boolean(isLoggedIn)
+//console.log(booleanIsLoggedIn);
+
+
+//"" => false
+// "sudhanshu" => true
+// 1 => true ; 0 => false
+
+let someNumber =22
+
+let stringNumber = String(someNumber);
+console.log(stringNumber);
+console.log(typeof stringNumber);
--- README.md
+++ README.md
@@ -1,2 +1,2 @@
 # JS-Rev
-JavaScript Re
+JavaScript Revision
