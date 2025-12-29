5694ab4518fed0c75cac1ca758b3802db42235a0
Sudhanshu Singh <sudhanshusingh04251@gmail.com>
2025-12-29 14:03:05 +0000

    container config done

---
+++ 01_basics/01_variables.js
@@ -0,0 +1,23 @@
+const accountId = 124233
+let accountEmail = "abcd@gmail.com"
+var accountPassword = "1234"
+accountCity = "Jaipur"
+let accountState ;
+
+// accountId = 2    not allowed 
+
+accountEmail = "124124124"
+accountPassword = "34234"
+accountCity = "Bengaluru"
+
+
+
+/*
+Prefer not to use var
+because of issue in block and functional scope
+*/ 
+
+
+
+
+console.log(accountId, accountEmail, accountPassword, accountCity, accountState);
---
+++ 01_basics/02_dataTypes.js
@@ -0,0 +1,31 @@
+"use strict";  // treat all JS code as newer version
+
+
+// alert(3+3) // we are using node js, not browser 
+
+let name = "sudhanshu"
+let age = 25
+let isLogeedIn = false
+let state;
+
+
+
+/*
+Primitive
+
+//number => 2 to power 53
+//bigint
+//string => " "
+//boolean => true/false
+//null => standalone value
+//undefinedunique
+//symbol =>
+
+*/ 
+
+
+
+//object
+
+console.log(typeof undefined);  // undefined
+console.log(typeof null);  //object
