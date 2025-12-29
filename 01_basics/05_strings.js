4dc42a9be57a65572fe7907f5e069c92bab5aad2
Sudhanshu Singh <sudhanshusingh04251@gmail.com>
2025-12-29 16:08:02 +0000

    strings done

---
+++ 01_basics/05_strings.js
@@ -0,0 +1,37 @@
+const name = "sudhanshu"
+const repoCount = 5
+
+// console.log(name + repoCount + " sValue");
+
+// console.log(`hello my name is ${name} and my repo count is ${repoCount}`);
+
+const gameName = new String('sudhanshu-singh')
+
+// console.log(gameName[0]);
+// console.log(gameName.__proto__);
+
+
+
+// console.log(gameName.length);
+// console.log(gameName.toUpperCase());
+// console.log(gameName.charAt(2));
+// console.log(gameName.indexOf('a'));
+
+
+const newString = gameName.substring(0,4)
+// console.log(newString);
+
+const anotherString = gameName.slice(-9,4)
+// console.log(anotherString);
+
+const newString1 = "   sudhanshu     "
+// console.log(newString1);
+// console.log(newString1.trim());
+
+
+const url = "https://google.com/google%1"
+// console.log(url.replace('%', '-'));
+// console.log(url.includes('google'));
+
+
+console.log(gameName.split('-'))
