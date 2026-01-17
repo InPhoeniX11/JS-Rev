3ef513acefe590270e5bcc9567b4b85f5d476d77
Sudhanshu Singh <sudhanshusingh04251@gmail.com>
2026-01-17 17:41:14 +0000

    iffe explained

---
+++ 03_basics/04_iife.js
@@ -0,0 +1,17 @@
+// Immediately Invoked Function Expressions (IIFE)
+
+
+// (function one() {
+      // named IIFE
+//   console.log(`DB CONNECTED`);
+// })();                               // semi colon needed to end iife if we are executing another function through iife too
+
+
+// (funtion two(){
+//   console.log(`DB CONNECTED`)
+// }) ()                          
+
+((name) => {
+    // unnamed IIFE
+  console.log(`DB CONNECTED TWO ${name}`);
+})('sudhanshu')
