4ba27dc460cedb4893cf6eb4832bcff012ce3a20
Sudhanshu Singh <sudhanshusingh04251@gmail.com>
2026-01-22 11:32:01 +0000

    if else

---
+++ 04_control_flow/one.js
@@ -0,0 +1,34 @@
+//    if
+
+// if (2 === "2") {
+//   console.log("executed");
+// } else {
+//   console.log("not executed")
+// }
+
+// <, >, <=, >=, ==, !=, ===, !==
+
+
+const balance = 1000;
+
+// if (balance > 500) console.log('test'),
+//   console.log("test2");
+
+// if (balance < 500) {
+//   console.log("less than 500")
+// } else if (balance < 750) {
+//   console.log("lesss than 750")
+// } else if (balance < 1000) {
+//   console.log("less than 1000")
+// } else
+//   [
+//     console.log("less than 1200")
+//   ]
+
+
+const userLoggedIn = true;
+const debitCard = true;
+
+if (userLoggedIn && debitCard && 2 == 3) {
+  console.log("allow to buy course");
+}
