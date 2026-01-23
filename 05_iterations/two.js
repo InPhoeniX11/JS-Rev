d4901fb4e82d86b97e30a79fc4cab92731af08c2
Sudhanshu Singh <sudhanshusingh04251@gmail.com>
2026-01-23 15:14:57 +0000

    while loop

---
+++ 05_iterations/two.js
@@ -0,0 +1,27 @@
+// while
+
+
+let i =0;
+while(i<= 10){
+  // console.log(`Value of i is ${i}`);
+  i= i+2;
+}
+
+
+let myArray = ['Flash', 'Superman', 'Batman'];
+let arr = 0;
+while(arr<myArray.length){
+  // console.log(`Value is ${myArray[arr]}`);
+  arr = arr+1;
+}
+
+
+//  do-while
+
+
+let score = 11;
+
+do{
+  console.log(`Score is ${score}`)
+  score++;
+}while(score<=10);
