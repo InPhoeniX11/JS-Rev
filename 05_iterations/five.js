05fa1f659d02aa1f532f99067f151e0f01268df4
Sudhanshu Singh <sudhanshusingh04251@gmail.com>
2026-01-24 07:50:19 +0000

    for each loop

---
+++ 05_iterations/five.js
@@ -0,0 +1,35 @@
+const coding = ["js", "cpp", "java", "python"]
+
+// coding.forEach( function (val) {
+//   console.log(val);
+// })
+
+// coding.forEach( (val) => {
+//   console.log(val);
+// })
+
+// function printMe(item){
+  //   console.log(item);
+  // }
+  
+  // coding.forEach(printMe)
+
+
+  // coding.forEach( (val, index, arr) => {
+  //   console.log(val,index,arr);
+  // })
+
+  const myCoding = [
+    {
+      languageName: "JavaScript",
+      languageFile: "js"
+    },
+    {
+      languageName: "Java",
+      languageFile: "java"
+    },
+    {
+      languageName: "python",
+      languageFile: "py"
+    }
+  ]
